231c7e444482f819ffa1eaefb41ad18d93a1a56a
FedeOt <federico.lastra@onetree.com>
2022-09-06 10:41:52 -0300

    checking accounts without using user variable

--- src/components/CheckingAccountsList.jsx
+++ src/components/CheckingAccountsList.jsx
@@ -1,24 +1,23 @@
-import {useState,useEffect ,useContext} from 'react';
+import {useState,useEffect} from 'react';
 import { getAllCheckingAccounts } from '../api';
-import { myContext } from '../context/Authcontext';
 import { AccountCard } from './AccountCard';
 
 
 export const CheckingAccountsList = () => {
 
     
-    const {user} = useContext(myContext); 
+     
     const [checkingAccounts,setCheckingAccounts] = useState([]);
     const [loading,setLoading] = useState(true); 
 
     useEffect(()=>{
 
-      getAllCheckingAccounts(user).then(response => {
+      getAllCheckingAccounts().then(response => {
           setCheckingAccounts(response.data);
           setLoading(false); 
       })
 
-    },[user]); 
+    },[]); 
 
 
 
