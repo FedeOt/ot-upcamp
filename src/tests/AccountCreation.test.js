6a463e68e101ceb6ecd0ee8039c54ec4a35487d8
FedeOt <federico.lastra@onetree.com>
2022-09-12 08:14:16 -0300

    Create new account test

--- src/tests/AccountCreation.test.js
+++ src/tests/AccountCreation.test.js
@@ -1,42 +1,35 @@
 import '@testing-library/jest-dom/extend-expect'
-import {fireEvent, render} from '@testing-library/react'
-import axios from 'axios'
-import { CheckingAccountCreation } from '../components/CheckingAccountCreation'
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { postCheckingAccounts } from '../api';
+import {CheckingAccountCreation} from '../components/CheckingAccountCreation'
 
 //this test is not finished yet
+jest.mock('../api/index.js');
 
 test('Should create the checking account with the given info',async()=>{
 
-    const accountData = {
-        name:'testing account',
-        deposit:'2300'
+    const response = {
+      status:200
     }
+
+    postCheckingAccounts.mockResolvedValue(response); 
     
-    try{
-        const response = await axios.post(('http://localhost:8080/bank/api/v1/auth', {},
-        {
-          params: {
-            username: 'jsmith@demo.io',
-            password: 'Demo123!'
-          }
-        }
-        ));
-        sessionStorage.setItem('token',response.data.authToken); 
-    }catch(err){
+    const form = render(<CheckingAccountCreation/>)
 
-        console.log(err); 
+    const name = form.getByTestId('account-name');
+    const deposit = form.getByTestId('initial-deposit');
+    const submit = form.getByTestId('submit'); 
 
-    }
+    fireEvent.change(name,{target:{value:'testing'}}); 
+    fireEvent.change(deposit,{target:{value:'2500'}}); 
+    fireEvent.click(submit);  
+
+    await waitFor(()=>{
+      expect(form.getByTestId('success')).toBeInTheDocument(); 
+    })
     
-    const form = render(<CheckingAccountCreation/>); 
+  
+
 
     
-    const nameInput = form.getByTestId('account-name'); 
-    const depositInput = form.getByTestId('initial-deposit'); 
-    const submitBtn = form.getByTestId('submit'); 
-
-    fireEvent.change(nameInput,{target:{value:accountData.name}}); 
-    fireEvent.change(depositInput,{target:{value:accountData.deposit}});
-    fireEvent.click(submitBtn); 
-    expect(form.getByTestId('success')).toBeInTheDocument(); 
 })
