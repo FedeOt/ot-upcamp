9a3def8b66ca4b4dfa57020b6bd7c7f177597210
FedeOt <federico.lastra@onetree.com>
2022-10-06 12:39:51 -0300

    formated document

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -3,49 +3,42 @@ import axios from "axios";
 import { act } from "react-dom/test-utils";
 import { Login } from "../components/Login";
 
-
-
 const credentials = {
-    username:'jsmith@demo.io',
-    password:'Demo123!'
-}
-const testingToken = 'SOME_TOKEN'; 
-const testingRole = 'ROLE_USER';
+  username: "jsmith@demo.io",
+  password: "Demo123!",
+};
+const testingToken = "SOME_TOKEN";
+const testingRole = "ROLE_USER";
 
 const mockedUsedNavigate = jest.fn();
 
-jest.mock('axios'); 
+jest.mock("axios");
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-   useNavigate: () => mockedUsedNavigate,
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
 }));
 
+test("Should access into the app after login", async () => {
+  const login = render(<Login />);
 
+  axios.post.mockResolvedValue({ data: testingToken });
+  axios.get.mockResolvedValue({ data: [{ authority: testingRole }, ""] });
 
-test('Should access into the app after login',async()=>{
-    
-    const login = render(<Login/>);
-    
-    axios.post.mockResolvedValue({data:testingToken});  
-    axios.get.mockResolvedValue({data:[{authority:testingRole},'']}); 
+  const inputUsername = login.getByTestId("username");
+  const inputPassword = login.getByTestId("password");
+  const submitBtn = login.getByTestId("login-submit");
+  act(() => {
+    fireEvent.change(inputUsername, {
+      target: { value: credentials.username },
+    });
+    fireEvent.change(inputPassword, {
+      target: { value: credentials.password },
+    });
+    fireEvent.click(submitBtn);
+  });
 
-    const inputUsername = login.getByTestId('username');
-    const inputPassword = login.getByTestId('password'); 
-    const submitBtn = login.getByTestId('login-submit'); 
-    act(()=>{
-        fireEvent.change(inputUsername,{target:{value:credentials.username}}); 
-        fireEvent.change(inputPassword,{target:{value:credentials.password}}); 
-        fireEvent.click(submitBtn);
-    })
-     
-    
-    await waitFor(()=>{
-        expect(mockedUsedNavigate).toHaveBeenCalled(); 
-    })
-    
-
-
-
-
-})
+  await waitFor(() => {
+    expect(mockedUsedNavigate).toHaveBeenCalled();
+  });
+});
--- src/tests/Logout.test.js
+++ src/tests/Logout.test.js
@@ -1,29 +1,17 @@
 import { fireEvent, render } from "@testing-library/react";
 import { Logout } from "../components/Logout";
 
-
-
-
-
 const mockedUsedNavigate = jest.fn();
 
-
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-   useNavigate: () => mockedUsedNavigate,
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
 }));
- 
 
-test('Should redirect to the login after logout',()=>{
+test("Should redirect to the login after logout", () => {
+  const LogoutComponent = render(<Logout />);
+  const logOutBtn = LogoutComponent.getByTestId("logout");
+  fireEvent.click(logOutBtn);
 
-    const LogoutComponent = render(<Logout/>); 
-    const logOutBtn = LogoutComponent.getByTestId('logout'); 
-    fireEvent.click(logOutBtn); 
-
-    expect(mockedUsedNavigate).toHaveBeenCalledWith('/'); 
-
-})
-
-    
-
-
+  expect(mockedUsedNavigate).toHaveBeenCalledWith("/");
+});
