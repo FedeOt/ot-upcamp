e04dd1bab9ba80a6bbc0e0b8fa04402037939fd5
FedeOt <federico.lastra@onetree.com>
2022-10-20 08:57:53 -0300

    select account for deposit + select amount

--- src/components/Deposit.jsx
+++ src/components/Deposit.jsx
@@ -8,6 +8,8 @@ export const Deposit = () => {
 
     const [accounts,setAccounts] = useState([]); 
     const [loading,setLoading] = useState(true); 
+    const [currentAccount,setCurrentAccount] = useState(''); 
+    const [amount,setAmount] = useState(''); 
 
     useEffect(()=>{
         getAllCheckingAccounts()
@@ -18,20 +20,29 @@ export const Deposit = () => {
     })
 
     const handleSelectChange = ({target})=>{
-        console.log(target.value); 
+        
+        setCurrentAccount(target.value); 
     }
 
 
   return (
-    <div> 
-        <h3>Deposit</h3>
-        <select onChange={handleSelectChange}>
-            {
-                !loading && accounts.map((element,index)=> <option key={index} value={element.id}>{element.name}</option>)
-            }
+    <div className='p-5'> 
+        <div className='pt-5 border border-dark w-50 p-5 rounded' style={{marginLeft:450}}>
+            <h3>Deposit</h3>
+            <span>Select account</span>
+            <select className='form-select w-25' onChange={handleSelectChange}>
+                {
+                    !loading && accounts.map((element,index)=> <option key={index} value={element.id}>{element.name}</option>)
+                }
+            </select>
+            <div className='mt-4'>
+                <span className='mt-4'>Select amount</span>
+                <input type="text" className='form-control w-25' placeholder='Amount'  />
+            </div>
+
+            <button className='btn btn-success mt-4'>Deposit</button>
             
-            
-        </select>
+        </div>
 
     </div>
   )
